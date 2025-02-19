cd323cd4cc95f9045fb27c0ed762dcdcd091f40f
Umberto Tramontano <u.tramontano92@libero.it>
2025-02-19 16:55:38 +0100

    first commit

--- src/main/java/it/epicode/gestione_viaggi_aziendali/security/auth/JwtRequestFilter.java
+++ src/main/java/it/epicode/gestione_viaggi_aziendali/security/auth/JwtRequestFilter.java
@@ -1,7 +1,6 @@
 package it.epicode.gestione_viaggi_aziendali.security.auth;
 
 import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
